0bc51087f980c9a4a246e28b3a20adfbbe74b2e7
Ashrya Agrawal <40825065+ashryaagr@users.noreply.github.com>
2018-12-17 18:50:32 +0530

    Basic login process and signup process done

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
 import os
-from flask import Flask, render_template, request, flash, session, abort, redirect, jsonify
+from flask import Flask, render_template, request, flash, session, abort, redirect, jsonify, session, url_for
 from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
+import flask_login
 from models import *
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.login("ashryaagrdeveloper@gmail.com", "#####")
 
 app = Flask(__name__)
-#app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db.init_app(app)
 
 socketio = SocketIO(app)
 
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+def main():
+    db.create_all()
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if 'username' in session:
+        pass
+    else :
+        return render_template("index.html")
 
 @app.route("/sign_up")
 def sign_up():
@@ -26,17 +40,19 @@ def sign_up():
 def login():
     return render_template("login.html")
 
-@app.route("/account_creation", methods=["POST"])
+@app.route("/account_creation")
 def account_creation():
     name = request.form.get("InputDisplayName")
     email = request.form.get("InputEmail1")
     password = request.form.get("InputPassword1")
-    """ if email in db.email:
+    user = User(display_name=name, email=email, password=password)
+    if User.query.filter_by(email=email) :
         message = "Sorry the Email already exists in our DATABASE. Please try with some other Email"
         return render_template("account_creation.html", message=message)
-    else :
-        flash('account sucessfully created') """
-    return login()
+    message = "You have tried to register for the online messaging service : FLACK . I assure you that best possible services shall be provided to you ."
+    server.send_message("ashryaagrdeveloper@gmail.com", email, message)
+    flash('account sucessfully created')
+    return render_template("login.html")
 
-if __name__=="__main__" :
-    app.run()
+if __name__== "__main__" :
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -6,9 +6,26 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Flight(db.Model):
-    __tablename__ = "flights"
-    id = db.Column(db.Integer, primary_key=True)
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     display_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
